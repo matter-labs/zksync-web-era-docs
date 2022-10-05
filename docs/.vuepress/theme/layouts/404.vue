175e7db0f5c912a4a4c0f318297472e09093f691
Levon <levonsargsyandev@gmail.com>
2022-10-05 18:37:05 +0400

    fix: incorrect-url-opened-after-clicking-on-take-me-home-link-on-404-page

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="theme-default-content">
+    <h1>404</h1>
+    <blockquote>{{ getMsg() }}</blockquote>
+    <RouterLink to="/dev/">Take me home.</RouterLink>
+  </div>
+</template>
+<script>
+const msgs = [`There's nothing here.`, `How did we get here?`, `That's a Four-Oh-Four.`, `Looks like we've got some broken links.`];
+export default {
+  methods: {
+    getMsg() {
+      return msgs[Math.floor(Math.random() * msgs.length)];
+    },
+  },
+};
+</script>
