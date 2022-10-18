65572355b93219d9cb3fae5bff6ae7cfee97aae0
Blessing Krofegha <bkrofegha@gmail.com>
2022-10-18 16:38:44 +0100

    add zkevm path to config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -148,7 +148,7 @@ module.exports = {
           path: "/api/tools/block-explorer", // optional, which should be a absolute path.
           collapsable: false, // optional, defaults to true
           sidebarDepth: 1, // optional, defaults to 1
-          children: ["/api/tools/block-explorer/intro", "/api/tools/block-explorer/block-view", "/api/tools/block-explorer/contract-verification"],
+          children: ["/api/tools/block-explorer/intro", "/api/tools/block-explorer/block-view", "/api/tools/block-explorer/contract-verification", "/api/tools/block-explorer/zkevm.md"],
         },
       ],
     }
