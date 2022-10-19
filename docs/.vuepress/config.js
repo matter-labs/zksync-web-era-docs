c6cb4818e457527fd239cc1b41ca4bb32768c0e2
Blessing Krofegha <bkrofegha@gmail.com>
2022-10-19 12:46:58 +0100

    include contract verification path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -68,6 +68,7 @@ module.exports = {
             "/dev/developer-guides/hello-world.md",
             "/dev/developer-guides/contracts/system-contracts.md",
             "/dev/developer-guides/contracts/contracts.md",
+            "/api/tools/block-explorer/contract-verification.md",
             "/dev/developer-guides/transactions/transactions.md",
             "/dev/developer-guides/transactions/blocks.md",
             "/dev/developer-guides/transactions/aa.md",
