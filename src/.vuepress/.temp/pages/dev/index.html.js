c6f9ee8f19123c49797ef5b75b4466f035a3ec6d
Blessing Krofegha <bkrofegha@gmail.com>
2023-01-03 17:46:36 +0100

    stash changes

---
+++ src/.vuepress/.temp/pages/dev/index.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-7445cd33\",\"path\":\"/dev/\",\"title\":\"zkSync Documentation\",\"lang\":\"en-US\",\"frontmatter\":{\"description\":\"This documentation is intended to assist you in developing on zkSync. It introduces the concepts of zkSync, describes the zkSync network stack, and some advanced topics for comp...\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://v2-docs.zksync.io/dev/\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"zkSync Documentation\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"This documentation is intended to assist you in developing on zkSync. It introduces the concepts of zkSync, describes the zkSync network stack, and some advanced topics for comp...\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}]]},\"headers\":[{\"level\":2,\"title\":\"Fundamental topics\",\"slug\":\"fundamental-topics\",\"link\":\"#fundamental-topics\",\"children\":[{\"level\":3,\"title\":\"Developer guides\",\"slug\":\"developer-guides\",\"link\":\"#developer-guides\",\"children\":[]},{\"level\":3,\"title\":\"Tools and SDKs\",\"slug\":\"tools-and-sdks\",\"link\":\"#tools-and-sdks\",\"children\":[]},{\"level\":3,\"title\":\"Examples and tutorials\",\"slug\":\"examples-and-tutorials\",\"link\":\"#examples-and-tutorials\",\"children\":[]}]}],\"readingTime\":{\"minutes\":1.99,\"words\":598},\"filePathRelative\":\"dev/README.md\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
