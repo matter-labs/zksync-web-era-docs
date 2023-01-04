224070f6b68aa413c30e37f39e76a6d26e0a42b5
Blessing Krofegha <bkrofegha@gmail.com>
2023-01-04 10:05:45 +0100

    updated files

---
+++ docs/.vuepress/.temp/pages/dev/developer-guides/building-on-zksync/videos.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-2f5ccb55\",\"path\":\"/dev/developer-guides/building-on-zksync/videos.html\",\"title\":\"Video resources\",\"lang\":\"en-US\",\"frontmatter\":{\"description\":\"zkSync 2.0 for Builders - Alex Gluchowski This talk covers everything you need to know about zkSync 2.0, and the user, and developer experience perspectives when building on zkS...\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://v2-docs.zksync.io/dev/developer-guides/building-on-zksync/videos.html\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Video resources\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"zkSync 2.0 for Builders - Alex Gluchowski This talk covers everything you need to know about zkSync 2.0, and the user, and developer experience perspectives when building on zkS...\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}]]},\"headers\":[{\"level\":2,\"title\":\"zkSync 2.0 for Builders - Alex Gluchowski\",\"slug\":\"zksync-2-0-for-builders-alex-gluchowski\",\"link\":\"#zksync-2-0-for-builders-alex-gluchowski\",\"children\":[]},{\"level\":2,\"title\":\"The zkEVM is Here - Steve Newcomb\",\"slug\":\"the-zkevm-is-here-steve-newcomb\",\"link\":\"#the-zkevm-is-here-steve-newcomb\",\"children\":[]},{\"level\":2,\"title\":\"zkEVM: Compatibility vs Equivalence - Alex Gluchowski\",\"slug\":\"zkevm-compatibility-vs-equivalence-alex-gluchowski\",\"link\":\"#zkevm-compatibility-vs-equivalence-alex-gluchowski\",\"children\":[]}],\"readingTime\":{\"minutes\":0.68,\"words\":204},\"filePathRelative\":\"dev/developer-guides/building-on-zksync/videos.md\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
