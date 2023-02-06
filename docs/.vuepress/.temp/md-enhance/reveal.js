d89602f2068477662d5dc4df8f33355c51b48d8e
Blessing Krofegha <bkrofegha@gmail.com>
2023-02-06 18:12:47 +0100

    Added warning messages for ecrecover and tx.origin respectively (#268)
    * update .gitignore
    
    * remove files
    
    * added warning message for ecrecover and tx.origin
    
    * changes format
    
    ---------
    
    Co-authored-by: Antonio <aug@matterlabs.dev>

--- docs/.vuepress/.temp/md-enhance/reveal.js
+++
@@ -1,3 +0,0 @@
-import { reveal, revealMarkdown, revealHighlight, revealMath, revealSearch, revealNotes, revealZoom } from "/Users/beveloper/Desktop/projects/zksync-web-v2-docs/node_modules/vuepress-plugin-md-enhance/lib/client/reveal/index.js";
-
-export const useReveal = () => [reveal(), revealMarkdown(), revealHighlight(), revealMath(), revealSearch(), revealNotes(), revealZoom()];
