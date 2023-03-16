35c59a0dbcf532803583f76fb97be92a3d8355ea
Blessing Krofegha <bkrofegha@gmail.com>
2023-03-16 19:48:30 +0100

    added nav and categorized the addresses

--- docs/.vuepress/sidebar/en.ts
+++ docs/.vuepress/sidebar/en.ts
@@ -62,11 +62,11 @@ export const enSidebar = sidebar({
       link: "/dev/troubleshooting",
       children: [
         "/dev/troubleshooting/changelog.md",
+        "/dev/troubleshooting/usefull-address.md",
         "/dev/troubleshooting/audit-bug-bounty.md",
         "/dev/troubleshooting/docs-contribution/docs.md",
         "/dev/troubleshooting/docs-contribution/community-resources.md",
         "/dev/troubleshooting/faq.md",
-        "/dev/troubleshooting/usefull-address.md",
         // "/dev/troubleshooting/status.md",
         // "/dev/troubleshooting/important-links.md",
       ],
