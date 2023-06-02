b40853c33f7d60d8673efee8b159815fa2d87467
Blessing Krofegha <bkrofegha@gmail.com>
2023-06-02 16:22:40 +0100

    updated the css

--- docs/.vuepress/components/NavBar.vue
+++ docs/.vuepress/components/NavBar.vue
@@ -1,20 +1,56 @@
     <template>
         <NavBar>
         <template #rightStart>
-            <GoogleTranslateSelect default-language-code="en" default-page-language-code="en" :fetch-browser-language="false" trigger="click" @select="handleGoogleTranslateSelect" />
+            <GoogleTranslateSelect class="google-translate-select-language" default-language-code="en" default-page-language-code="en" :fetch-browser-language="false" @select="handleGoogleTranslateSelect" />
         </template>
         <template #centerStart>
-            <div class="bar">
-                <p class="text">Our language translation is not working properly.</p>
+            <div class="bar" :class="{ hide: !showBar }">
+            <p class="text">Our language translation is not working properly.</p>
             </div>
         </template>
         </NavBar>
     </template>
     
     <script setup lang="ts">
+    import { ref, onMounted, onBeforeUnmount } from 'vue';
     import NavBar from "vuepress-theme-hope/modules/navbar/components/Navbar";
     import GoogleTranslateSelect from "@google-translate-select/vue3";
     
     const handleGoogleTranslateSelect = () => {};
+    
+    const showBar = ref(false);
+    
+    // Attach a click event listener to the document
+    onMounted(() => {
+        const handleDocumentClick = (event) => {
+        const translateElement = document.querySelector(".google-translate-select-language");
+        const barElement = document.querySelector(".bar");
+    
+        console.log('Translate Element:', translateElement);
+        console.log('Bar Element:', barElement);
+        console.log('Before showBar:', showBar.value);
+    
+        // Check if the clicked element is the translate element
+        if (translateElement && event.target === translateElement) {
+            showBar.value = !showBar.value; // Toggle the visibility of the bar element
+        } else {
+            showBar.value = false; // Hide the bar element
+        }
+    
+        console.log('After showBar:', showBar.value);
+        };
+    
+        document.addEventListener("click", handleDocumentClick);
+    
+        // Clean up the event listener when the component is unmounted
+        onBeforeUnmount(() => {
+        document.removeEventListener("click", handleDocumentClick);
+        });
+    });
     </script>
     
+    <style scoped>
+    .bar.hide {
+        display: none;
+    }
+    </style>
