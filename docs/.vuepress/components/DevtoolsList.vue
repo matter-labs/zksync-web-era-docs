449d521fabe442628141a958135e2c793d453af9
Antonio <tonioufa@gmail.com>
2023-08-04 10:09:02 +0100

    fix: moves list to json file

--- docs/.vuepress/components/DevtoolsList.vue
+++ docs/.vuepress/components/DevtoolsList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-for="group in devtools" :key="group.category">
+  <div v-for="group in allTools.toolsByCategory" :key="group.category">
     <h3>{{ group.category }}</h3>
     <div class="flex-container">
-      <a :href="tool.url" class="flex-item" v-for="tool in category.tools" :key="tool.name">
+      <a :href="tool.url" class="flex-item" v-for="tool in group.tools" :key="tool.name">
         <p>{{ tool.name }}</p>
       </a>
     </div>
@@ -10,88 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
-const devtools = ref([
-  {
-    category: "frameworks",
-    tools: [
-      {
-        name: "Hardhat",
-        category: "Framework",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-      {
-        name: "Foundry",
-        category: "Framework",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-      {
-        name: "Foundry",
-        category: "Framework",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-      {
-        name: "Foundry",
-        category: "Framework",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-    ],
-  },
-  {
-    category: "tooling",
-    tools: [
-      {
-        name: "Thirdweb",
-        category: "Dev tools",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-      {
-        name: "Foundry",
-        category: "Framework",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-      {
-        name: "Atlas ZK",
-        category: "Dev tools",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-    ],
-  },
-  {
-    category: "oracles",
-    tools: [
-      {
-        name: "API3",
-        category: "Oracle",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-      {
-        name: "Redstone",
-        category: "Oracle",
-        description: "some desc",
-        logo: "",
-        url: "#",
-      },
-    ],
-  },
-]);
+import allTools from "../../assets/data/devtools.json";
 </script>
 
 <style scoped>
@@ -103,20 +22,22 @@ const devtools = ref([
   margin: 2rem auto;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: center;
+  /* justify-content: space-around; */
 }
 
 .flex-item {
+  transition: all 0.3s ease-out;
   /* background:  */
   border-radius: 8px;
   border: 1px solid #333;
-  /* color: white; */
   font-weight: bold;
   /* font-size: 2rem; */
   text-align: center;
   padding: 0.5rem;
   margin: 0.5rem;
-  flex: 1 1 auto;
+  /* flex: 1 1 auto; */
+  flex: 1 1 1;
   @media screen and (min-width: 320px) {
     width: 100%;
   }
--- docs/tools/README.md
+++ docs/tools/README.md
@@ -1,13 +1,17 @@
 # zkSync Era Tools
 
-## Official tools
+<!-- ## Official tools
 
 - [zkSync CLI](./zksync-cli/)
 - [Hardhat plugins](./hardhat/)
 - [Compiler toolchain](./compiler-toolchain/)
 - [Local testing](./testing/)
-- [Block explorer](./block-explorer/)
+- [Block explorer](./block-explorer/) -->
 
 ## Popular tools to build on zkSync Era
+
+::: info Adding tools
+Did we miss a tool or project? Add it to the list by [creating a PR in our docs](https://github.com/matter-labs/zksync-web-era-docs/compare).
+:::
 
 <DevtoolsList />
