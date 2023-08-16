1923e82f12a0c12066d3d7c19aea6145faf83e97
Antonio <tonioufa@gmail.com>
2023-08-16 12:29:31 +0100

    fix: alignement, adds descriptions and drpc

--- docs/.vuepress/components/DevtoolsList.vue
+++ docs/.vuepress/components/DevtoolsList.vue
@@ -5,11 +5,15 @@
     </h2>
     <div class="grid-container">
       <div class="grid-item" v-for="tool in group.tools" :key="tool.name">
-        <img :src="$withBase(`/images/tools/${tool.logo}`)" class="devlogo" v-if="tool.logo" :alt="`${tool.name} logo`" />
-        <h4 class="tool-name">
-          {{ tool.name }}
-        </h4>
-        <p v-if="tool.description" class="tool-desc">{{ tool.description }}</p>
+        <div class="logo-wrapper">
+          <img :src="$withBase(`/images/tools/${tool.logo}`)" class="devlogo" v-if="tool.logo" :alt="`${tool.name} logo`" />
+        </div>
+        <div class="content-wraper">
+          <h4 class="tool-name">
+            {{ tool.name }}
+          </h4>
+          <p v-if="tool.description" class="tool-desc">{{ tool.description }}</p>
+        </div>
         <span v-if="tool.soon" class="tool-desc">(soon)</span>
         <div class="btn-wrapper">
           <a :href="tool.url" target="_blank" class="button">Visit website </a>
@@ -62,12 +66,22 @@ import allTools from "../../assets/data/devtools.json";
   -moz-box-shadow: 4px 10px 24px -10px rgba(0, 0, 0, 0.75);
   box-shadow: 4px 10px 24px -10px rgba(0, 0, 0, 0.75);
 }
-
+.logo-wrapper {
+  height: 100px;
+  padding: 10px;
+}
 .devlogo {
   margin: 0.5rem auto;
-  width: 60%;
+  /* width: 100%; */
+  height: 100%;
   pointer-events: none !important;
   cursor: default !important;
+}
+.content-wraper {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 }
 .tool-name {
   margin-bottom: 0;
@@ -89,6 +103,6 @@ import allTools from "../../assets/data/devtools.json";
   font-weight: 500;
   color: #fff;
   background-color: #1e69ff;
-  border-radius: 10px;
+  border-radius: 8px;
 }
 </style>
