0299dd40ade4bad2fead36811a23f26ae81e3bd7
Ramon "9Tails" Canales <emaildoramon@gmail.com>
2024-01-18 10:08:24 +0000

    fix: putting code back that shouldn't have been removed

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -87,9 +87,20 @@ export default defineUserConfig({
         let contributors = document.querySelectorAll('.contributor');
         let contributorArr = Array.from(contributors);
         let topFive = contributorArr.slice(0, 5);
-        console.log(topFive, contributorArr)
       
-        
+        topFive.forEach(function(contributor) {
+          contributor.textContent = contributor.textContent.replace(',', '');
+        });
+      
+        let lastComma = contributorArr[4];
+        lastComma.textContent = lastComma.textContent.replace(',', '');
+      
+        let updatedList = topFive.map(function(contributor) {
+          return contributor.textContent;
+        }).join(', ');
+          
+        let contributorsDiv = document.querySelector('.contributors');
+        contributorsDiv.innerHTML = '<span class="label">Contributors: </span>' + updatedList;
       
       });
 
